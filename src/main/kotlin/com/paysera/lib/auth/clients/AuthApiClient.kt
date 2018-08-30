54b4cce5d56d85872b237f94afea2f890fd9d734
Vytautas Gimbutas <vytautas@gimbutas.net>
2018-08-30 11:38:14 +0300

    Observable changed to Single

--- src/main/kotlin/com/paysera/lib/auth/clients/AuthApiClient.kt
+++ src/main/kotlin/com/paysera/lib/auth/clients/AuthApiClient.kt
@@ -3,15 +3,15 @@ package com.paysera.lib.auth.clients
 import com.paysera.lib.auth.entities.SystemToken
 import com.paysera.lib.auth.entities.requests.CreateSystemTokenOptionalRequest
 import com.paysera.lib.auth.retrofit.APIClient
-import io.reactivex.Observable
+import io.reactivex.Single
 
 class AuthApiClient(private val apiClient: APIClient) {
 
-    fun invalidateAuthToken(authToken: String): Observable<Unit> {
+    fun invalidateAuthToken(authToken: String): Single<Unit> {
         return apiClient.invalidateAuthToken("Bearer ${authToken}")
     }
 
-    fun createSystemTokenOptional(authToken: String, audience: String, scope: String): Observable<SystemToken> {
+    fun createSystemTokenOptional(authToken: String, audience: String, scope: String): Single<SystemToken> {
         return apiClient.createSystemTokenOptional(
             "Bearer ${authToken}",
             CreateSystemTokenOptionalRequest(audience, scope)
