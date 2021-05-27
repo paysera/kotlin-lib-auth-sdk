9862077095cd7d6c0127ae88b38cb58b4a528e1d
GIlbert Parreño <gilbertparreno95@gmail.com>
2021-05-27 10:39:05 +0800

    migrate to kotlin-dsl and set up for maven central (#10)

---
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -0,0 +1,10 @@
+object ApplicationConfiguration {
+    const val artifactId = "kotlin-lib-auth-sdk"
+    const val groupId = "com.paysera.lib"
+    const val version = "3.8.1-SNAPSHOT"
+    const val name = "Kotlin Library Authentication SDK"
+    const val url = "https://www.paysera.com/v2/en-GB/index"
+    const val description = "Paysera: Kotlin Authentication SDK Library"
+    const val releaseRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/releases/"
+    const val snapshotRepositoryUrl = "https://s01.oss.sonatype.org/content/repositories/snapshots/"
+}
