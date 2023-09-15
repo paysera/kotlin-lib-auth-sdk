c06612bfadb0a9c3d1b4a5b2b0be75f94cc427c5
Stanislav Georgiev <104416551+s-k-georgiev@users.noreply.github.com>
2023-09-15 04:15:27 +0300

    APP-5717: [Android] Show correlation_id on screen in case of 5xx status code (#20)
    * APP-5717: [Android] Show correlation_id on screen in case of 5xx status code
    
    * Add snapshot url
    
    * Remove snapshot

--- buildSrc/src/main/kotlin/ApplicationConfiguration.kt
+++ buildSrc/src/main/kotlin/ApplicationConfiguration.kt
@@ -1,7 +1,7 @@
 object ApplicationConfiguration {
     const val artifactId = "kotlin-lib-auth-sdk"
     const val groupId = "com.paysera.lib"
-    const val version = "3.8.10"
+    const val version = "3.8.11"
     const val name = "Kotlin Library Authentication SDK"
     const val url = "https://www.paysera.com/v2/en-GB/index"
     const val description = "Paysera: Kotlin Authentication SDK Library"
